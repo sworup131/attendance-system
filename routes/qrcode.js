7f86640e91a3ee53937cd418eb58b58506894fac
sworup subedi <sworupsubedi@sworups-MacBook.local>
2025-11-02 22:59:22 -0600

    routes added

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -2,15 +2,26 @@ QRcode = require('qrcode')
 express = require('express')
 router = express.Router()
 
-//making qr code
-router.get('/', async(req,res)=>{
-    try{
-        url = await QRcode.toDataURL('www.youtube.com')
-        res.render('login', {qr: url})
-    }
-    catch{
-        console.log("api error")
-    }
+
+router.get('/',(req,res)=>{
+    res.redirect('/login')
 })
+router.get('/login',(req,res)=>{
+    res.render('login')
+})
+//making qr code
+// router.get('/', async(req,res)=>{
+//     try{
+//         url = await QRcode.toDataURL('www.youtube.com')
+//         res.render('login', {qr: url})
+//     }
+//     catch{
+//         console.log("api error")
+//     }
+// })
+
+// router.post('/login',(req,res)=>{
+
+// })
 
 module.exports = router;
